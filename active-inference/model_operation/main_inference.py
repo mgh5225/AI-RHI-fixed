74184a250b40ff0d4c830ec4ae4dcb879fafb900
Mohammad Ghazanfari <mgh.5225@gmail.com>
2023-05-13 22:15:56 +0330

    add prefix for mode_name

--- active-inference/model_operation/main_inference.py
+++ active-inference/model_operation/main_inference.py
@@ -12,57 +12,9 @@ from unity.environment import UnityContainer
 
 editor_mode = 0
 model_id = "vae"
-
+mode_prefix = ''
 log_id = "test"
 log_path = os.path.join(os.path.dirname(__file__), "operation_logs/")
-
-
-def reaching_tasks(model, position_import_model_id=None):
-    ids = [model + "reachclose", model + 'reachfar']
-
-    positions = np.zeros((2, 40, 2))
-    if position_import_model_id is None:
-        positions[0] = np.random.uniform(low=1, high=8, size=(
-            40, 2))*(-2 * np.random.randint(low=0, high=2, size=(40, 2)) + 1)
-        positions[1] = np.random.uniform(low=8, high=18, size=(
-            40, 2))*(-2 * np.random.randint(low=0, high=2, size=(40, 2)) + 1)
-    else:
-        positions[0] = CSVLogger.import_log(log_id=position_import_model_id + "reach" + "close" +
-                                            "0n", n=40, length=5000,
-                                            columns=["rubb_Shoulder", "rubb_Elbow"])[:, 0, :]
-
-        positions[1] = CSVLogger.import_log(log_id=position_import_model_id + "reach" + "far" +
-                                            "0n", n=40,  length=5000,
-                                            columns=["rubb_Shoulder", "rubb_Elbow"])[:, 0, :]
-
-    n_iterations = 5000
-
-    for i in range(0, 40):
-        for c in range(positions.shape[0]):
-            unity = UnityContainer(editor_mode)
-            unity.initialise_environment()
-
-            visual_decoder = VAE_CNN()
-            visual_decoder.load_from_file(model_id)
-
-            unity.set_visible_arm(VisibleArm.RealArm)
-            unity.reset()
-
-            unity.set_rubber_arm_rotation(np.array([[-17, -10]]))
-
-            # Set real arm to rubber arm position to get attr image
-            unity.set_rotation(unity.get_rubber_joint_observation())
-            attr_image = np.squeeze(unity.get_visual_observation())
-            unity.set_rotation(np.zeros((1, 2)))
-
-            data_range = np.load(
-                visual_decoder.SAVE_PATH + "/" + model_id + "/data_range" + model_id + ".npy")
-            central = FepAgent(unity, visual_decoder, data_range,
-                               enable_action=True, attractor_image=attr_image)
-            log_id = ids[c] + str(central.sp_noise_variance) + "n" + str(i)
-
-            central.run_simulation(log_id, log_path, n_iterations)
-            unity.close()
 
 
 def rhi_task(condition: Condition, stimulation: Stimulation):
@@ -81,7 +33,7 @@ def rhi_task(condition: Condition, stimulation: Stimulation):
                          model_id + "/data_range" + model_id + ".npy")
     agent = FepAgent(unity, visual_decoder, data_range, enable_action=False)
 
-    mode_name = f"{condition.name.lower()}_{stimulation.name.lower()}"
+    mode_name = f"{mode_prefix}{condition.name.lower()}_{stimulation.name.lower()}"
 
     agent.run_simulation(log_id, log_path, n_iterations, mode_name)
     unity.close()
@@ -113,7 +65,7 @@ def full_rhi_task(condition: Condition, stimulation: Stimulation, with_mu: bool,
 
     agent = FepAgent(unity, visual_decoder, data_range, enable_action=False)
 
-    mode_name = f"{condition.name.lower()}_{stimulation.name.lower()}"
+    mode_name = f"{mode_prefix}{condition.name.lower()}_{stimulation.name.lower()}"
 
     agent.run_simulation(log_id, log_path, n_iterations, mode_name)
 
